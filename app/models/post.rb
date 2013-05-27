13516bd54e7f567cd84fed3274318cf7c6853a5e
Jurgens Smit <contact@jurgenssmit.com>
2013-05-27 19:28:42 +1000

    initial commit

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ActiveRecord::Base
+	validates_presence_of :body, :title
+	attr_accessible :body, :title
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickBlog::Application.config.secret_token = 'b83c8947df57e6682696f134f387db86445918c310314cd062adb41d7a55dde217ce5f76f3a4b18d0ece982222601414a08440abb77fae7042e079655944b224'
